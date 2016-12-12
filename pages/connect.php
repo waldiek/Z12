d2875f41e5c5405775824f5b0b422742d9c0818d
waldiek <waldiek@gmail.com>
2016-12-12 15:27:19 +0100

    Update connect.php

--- pages/connect.php
+++ pages/connect.php
@@ -1,7 +1,1 @@
-<?php
-				// parametry serwera 
-				$serwer = "mysql3.superhost.pl";  // nazwa serwera mysql 
-				$login  = "sh181891_waldi";  // login do bazy 
-				$haslo  = "qweqwe123";  // haslo do bazy 
-				$baza   = "sh181891_waldi";  // nazwa bazy 
-?>
+
